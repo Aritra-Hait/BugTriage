8d5361f47a38860b09a9c9a776282c8060782961
Aritra-Hait <aritragames016@gmail.com>
2026-03-08 16:07:29 +0530

    prepared for the deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,16 +3,31 @@ import bugRoutes from "./routes/bugRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import teamRoutes from "./routes/teamRoutes.js";
 import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config();
 const app = express();
 
-
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
+const __dirname = path.resolve();
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+        credentials: true
+    }));
+}
 app.use(express.json());
 
 app.use("/api/bugs", bugRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/teams", teamRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.use((req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
 export default app;
